935cd1cdba270b5eae324bf12cefa5fe78a77777
Amber Charitos <amber.charitos@canonical.com>
2024-04-08 17:11:49 +0100

    add promtail-bin and check for server.log file before tailing

--- local-files/trino-entrypoint.sh
+++ local-files/trino-entrypoint.sh
@@ -3,5 +3,8 @@
 export PATH="${JAVA_HOME}/bin:${PATH}"
 usr/lib/trino/bin/launcher stop
 usr/lib/trino/bin/launcher start
-sleep 2
+
+while [ ! -f /data/trino/var/log/server.log ]; do
+    sleep 1
+done
 tail -f /data/trino/var/log/server.log
